d072af07fd663fb4092494acc3291d09dc60703f
klaas van der meulen <vandermeulen.k@gmail.com>
2016-06-06 02:33:35 +0200

    master: initial java maven angularjs setup

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Goals
 Maven as the build tool
 Java EE 7 as the back-end language
 Spring 1.4.0 instead of CDI - Contexts and Dependency Injection
-AngularJS as the frond-end framework
+AngularJS 1.5.6 as the frond-end framework
 Frond-end testing during the maven build
-Bower as the web package manager.
+Bower as the web package download manager to install components.
 Use GitHub as version control
 Bootstrap als css standard
 Javascript in src/main/javascript
@@ -15,11 +15,14 @@ Jasmine as javascript unit test tool integrated with maven
 
 Prerequisites
 
-Firefox or Chrome
-npm
-nodejs v4.4.5 as JavaScript runtime
-JDK
-IntelliJ IDE
-bower
-PhantomJs or brew install phantomjs
-Application Server or brew install glassfish
+Browser - Mozilla Firefox v46.0.1
+Project Management Tool - Maven v3.3.9
+IDE - IntelliJ IDEA Community Edition 2016 1.2 IDE
+Git - Version control system v2.6.1.windows.1
+Maven - Project Management Tool v3.3.9
+Javascript runtime - nodejs v4.4.5 (dl from nodejs.org)
+Package Manager for various dev stuff - npm v2.15.5 (comes with nodejs)
+Web Package Manager for the runtime - Bower v1.7.9
+JDK - Java SE Development Kit 8
+PhantomJs 
+Application Server 
---
+++ script-for-basic-maven-project.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# run this script with bash <scriptname>.sh
+#
+# 1: Archetype is a Maven project templating toolkit - to create a new project based on an archetype 
+#    - use mvn archetype:generate
+#	 - use eg. -DarchetypeArtifactId=mamaven-archetype-webapp to generate a sample Maven Webapp project
+#    - example: mvn archetype:generate -DgroupId=nl.kinked -DartifactId=CardGamesAPI -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+#
+echo "make archetype for webapp"
+mvn archetype:generate -DgroupId=nl.kinked -DartifactId=CardGamesAPI -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+#
+# 2: Add a java folder structure to the project  
+#    - mkdir for structure
+#    - touch to create empty files
+#	 - remove unnecessary files
+echo "make java folder structure and initial js files"
+mkdir -p src/main/java
+mkdir -p src/test/java
+mkdir -p src/test/javascript/unit
+mkdir -p src/test/javascript/e2e
+mkdir -p src/test/resources
+rm -f ./src/main/webapp/WEB-INF/web.xml
+rm -f ./src/main/webapp/index.jsp
+mkdir -p ./src/main/webapp/css
+touch ./src/main/webapp/css/specific.css
+mkdir -p ./src/main/webapp/js
+touch ./src/main/webapp/js/app.js
+touch ./src/main/webapp/js/controllers.js
+touch ./src/main/webapp/js/routes.js
+touch ./src/main/webapp/js/services.js
+touch ./src/main/webapp/js/filters.js
+touch ./src/main/webapp/js/services.js
+mkdir -p ./src/main/webapp/vendor
+mkdir -p ./src/main/webapp/partials
+mkdir -p ./src/main/webapp/img
+touch .bowerrc
+#
+# 3: Initialize package.json to let npm know the name of your package and dependencies 
+#    - npm init - to create a package.json for npm to handle the project's dependencies 
+#    - cat package.json - to show the just created file
+#    - tweak the package.json - add dev-dependencies and scripts for karma*, protractor
+#
+npm init
+#
+# 4: Tell bower to download packages not to bower-components but to a vendor folder
+#    - tweak the bowerrc file
+#    - add the directory vendor
+#    - use bower to install angular and bootstrap dependencies
+#    - bootstrap is a html, css and JS framework for developing mobile-first web sites
+#
+bower install angular
+bower install angular-route
+bower install angular-animate
+bower install angular-mocks
+bower install angular-loader
+bower install bootstrap
+#
+# 5: Initialize bower.json to let bower know the name of your package and dependencies 
+#    - bower init via CMD!!
+#    - npm via bower will fill the directory vendor automated after npm install!
+#
+bower init
+#
+# 6: Test the setup of bower; npm via bower will fill the directory vendor automated after npm install!
+#
+rm -rf ./src/main/webapp/vendor
+npm install
+#
+# 7: Add Karma as the testrunner for javascript
+#    - create the ./src/test/javascript/karma.conf.js containing a link to angular-mocks.js and correct plugins
+#    - this will run the unit tests by using the command npm test
+#
+# 8: Use protractor as the end-2-end tester.
+#    - create the ./src/test/javascript/protractor.conf.js mentioning jasmine as framework
+#    - this will run the integration test by using the command npm run protractor
+#
+#
+# 9: Add an index.html and the js files and the jasmine and e2e tests
+#    - start a terminal window in the root of the project and start the test server
+#
+
